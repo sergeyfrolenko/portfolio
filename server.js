92a92c57abcea221e3e1208c77d280beec9fbca6
sergeyfrolenko <sergeyfrolenko@ro.ru>
2020-06-18 20:49:28 +0500

    handler forms

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const upload = multer();
 
+// USE section
 const things = require('./things');
 app.use('/things', things);
 
+app.use(express.static('public'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(upload.array());
+
+// SET section
 app.set('view engine', 'pug');
 app.set('views', './views');
-app.use(express.static('public'))
 
+// Routing
 app.get('/', (req, res)=>{
    res.render('dynamic',{
      name: 'Olya',
@@ -23,7 +34,15 @@ app.get('/skills', (req, res)=>{
      logo: 'Knowledge light your future'
    });
 });
+app.get('/form', (req, res)=>{
+  res.render('form');
+});
+app.post('/form', (req, res)=>{
+  console.log(req.body);
+  res.send('recieved your request!');
+});
 
+// Listening
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
