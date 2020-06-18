76b6691270f8de01fca7c344e7c0b151f4b914d7
root <root@194-67-108-218.cloudvps.regruhosting.ru>
2020-06-18 09:42:49 +0300

    first commit

--- server.js
+++ server.js
@@ -1,2 +1,22 @@
 const app = require('express')();
 
+const things = require('./things');
+app.use('/things', things);
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.get('/', (req, res)=>{
+   res.render('dynamic',{
+     name: 'Olya',
+     url: 'webref'
+   });
+});
+app.get('/first', (req, res)=>{
+   res.render('first');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
